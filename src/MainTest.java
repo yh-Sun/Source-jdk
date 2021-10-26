185c9e69f11ad83405c99936adf845defd60989b
孙元贺 <sunyuanhe1@jd.com>
2021-10-26 10:49:39 +0800

    源码提交

---
+++ src/MainTest.java
@@ -0,0 +1,12 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.HashMap;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Integer i = 1;
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("1", 2);
+        System.out.println(map);
+    }
+}
